d2e196e88ee9fae96364f61767dfb3d01f5f6b76
Jung-won-seok <ukksj0621@naver.com>
2024-03-22 13:13:45 +0900

    commit

---
+++ js/results.js
@@ -0,0 +1,15 @@
+import {results, mbtis}from './data.js'
+
+const mbti = new URLSearchParams(location.search).get('mbti')
+console.log(mbti)
+const result = results[mbtis[mbti]]
+
+const titleEl = document.querySelector('.page-title')
+const characterEl = document.querySelector('.character')
+const boxEls = document.querySelectorAll('.box')
+const jobEls = document.querySelectorAll('.job')
+const lectureEl = document.querySelector('.lecture')
+const lectureImgEl = document.querySelector('.lecture img')
+
+titleEl.innerHTML = result.title
+characterEl.src = result.character
